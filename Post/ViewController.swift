078b674f42a8242efb970d19113c5cf5a4c5a45a
Xavier <xwatts2323@Xaviers-MacBook-Pro.local>
2018-10-15 11:53:57 -0600

    Initial commit

---
+++ Post/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Post
+//
+//  Created by Xavier on 10/15/18.
+//  Copyright © 2018 Xavier ios dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
